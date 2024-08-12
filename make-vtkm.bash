af779b493c9590987d44a298c238117727b4bbda
Kenneth Moreland <morelandkd@ornl.gov>
2024-08-12 14:36:23 -0400

    Setup build for VTK-m 2.2.0

--- make-vtkm.bash
+++ make-vtkm.bash
@@ -5,4 +5,4 @@ scriptdir=$(dirname $(realpath $0))
 . $scriptdir/setup-spack.sh
 
 spack concretize -f
-spack install --add vtk-m @2.1.0+rocm~openmp+examples+kokkos amdgpu_target=gfx90a
+spack install --add vtk-m @2.2.0+rocm~openmp+examples+kokkos amdgpu_target=gfx90a
